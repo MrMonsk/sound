e74d430ea808ee45b7752782cfb1b8ea739c1ba9
yoonster <yowonyoon@gmail.com>
2017-05-07 14:27:48 -0400

    add some constants

---
+++ constants/basic.js
@@ -0,0 +1,11 @@
+export const fancyModes = {
+    dorian: [0,2,3,5,7,9,10],
+    lydian: [0,2,4,6,7,9,11]
+}
+
+export const basicModes = {
+    major: [0,2,4,5,7,9,11,12],
+    minor: [0,2,3,5,7,8,10,12],
+    harmonicMinor: [0,2,3,5,7,8,11,12],
+    melodicMinor: [0,2,3,5,7,9,11,12]
+}
