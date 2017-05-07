8fa553a1413c8a10be5eca3ffc937d63de1bdb3e
yoonster <yowonyoon@gmail.com>
2017-05-07 14:23:27 -0400

    js notes

---
+++ constants/notes.js
@@ -0,0 +1,706 @@
+export default [
+  {
+    keyNumber: 1,
+    keyPosition: 0,
+    helmholtzName: "A͵͵ sub-contra-octave",
+    scientificName: "A0 Double Pedal A",
+    letterName: "a/0",
+    hzFrequency: 27.5
+  },
+  {
+    keyNumber: 2,
+    keyPosition: 0.5,
+    helmholtzName: "A♯͵͵/B♭͵͵",
+    scientificName: "A♯0/B♭0",
+    letterName: "a#/0,bb/0",
+    hzFrequency: 29.1352
+  },
+  {
+    keyNumber: 3,
+    keyPosition: 1,
+    helmholtzName: "B͵͵",
+    scientificName: "B0",
+    letterName: "b/0",
+    hzFrequency: 30.8677
+  },
+  {
+    keyNumber: 4,
+    keyPosition: 2,
+    helmholtzName: "C͵ contra-octave",
+    scientificName: "C1 Pedal C",
+    letterName: "c/1",
+    hzFrequency: 32.7032
+  },
+  {
+    keyNumber: 5,
+    keyPosition: 2.5,
+    helmholtzName: "C♯͵/D♭͵",
+    scientificName: "C♯1/D♭1",
+    letterName: "c#/1,db/1",
+    hzFrequency: 34.6478
+  },
+  {
+    keyNumber: 6,
+    keyPosition: 3,
+    helmholtzName: "D͵",
+    scientificName: "D1",
+    letterName: "d/1",
+    hzFrequency: 36.7081
+  },
+  {
+    keyNumber: 7,
+    keyPosition: 3.5,
+    helmholtzName: "D♯͵/E♭͵",
+    scientificName: "D♯1/E♭1",
+    letterName: "d#/1,eb/1",
+    hzFrequency: 38.8909
+  },
+  {
+    keyNumber: 8,
+    keyPosition: 4,
+    helmholtzName: "E͵",
+    scientificName: "E1",
+    letterName: "e/1",
+    hzFrequency: 41.2034
+  },
+  {
+    keyNumber: 9,
+    keyPosition: 5,
+    helmholtzName: "F͵",
+    scientificName: "F1",
+    letterName: "f/1",
+    hzFrequency: 43.6535
+  },
+  {
+    keyNumber: 10,
+    keyPosition: 5.5,
+    helmholtzName: "F♯͵/G♭͵",
+    scientificName: "F♯1/G♭1",
+    letterName: "f#/1,gb/1",
+    hzFrequency: 46.2493
+  },
+  {
+    keyNumber: 11,
+    keyPosition: 6,
+    helmholtzName: "G͵",
+    scientificName: "G1",
+    letterName: "g/1",
+    hzFrequency: 48.9994
+  },
+  {
+    keyNumber: 12,
+    keyPosition: 6.5,
+    helmholtzName: "G♯͵/A♭͵",
+    scientificName: "G♯1/A♭1",
+    letterName: "g#/1,ab/1",
+    hzFrequency: 51.9131
+  },
+  {
+    keyNumber: 13,
+    keyPosition: 7,
+    helmholtzName: "A͵",
+    scientificName: "A1",
+    letterName: "a/1",
+    hzFrequency: 55
+  },
+  {
+    keyNumber: 14,
+    keyPosition: 7.5,
+    helmholtzName: "A♯͵/B♭͵",
+    scientificName: "A♯1/B♭1",
+    letterName: "a#/1,bb/1",
+    hzFrequency: 58.2705
+  },
+  {
+    keyNumber: 15,
+    keyPosition: 8,
+    helmholtzName: "B͵",
+    scientificName: "B1",
+    letterName: "b/1",
+    hzFrequency: 61.7354
+  },
+  {
+    keyNumber: 16,
+    keyPosition: 9,
+    helmholtzName: "C great octave",
+    scientificName: "C2 Deep C",
+    letterName: "c/2",
+    hzFrequency: 65.4064
+  },
+  {
+    keyNumber: 17,
+    keyPosition: 9.5,
+    helmholtzName: "C♯/D♭",
+    scientificName: "C♯2/D♭2",
+    letterName: "c#/2,db/2",
+    hzFrequency: 69.2957
+  },
+  {
+    keyNumber: 18,
+    keyPosition: 10,
+    helmholtzName: "D",
+    scientificName: "D2",
+    letterName: "d/2",
+    hzFrequency: 73.4162
+  },
+  {
+    keyNumber: 19,
+    keyPosition: 10.5,
+    helmholtzName: "D♯/E♭",
+    scientificName: "D♯2/E♭2",
+    letterName: "d#/2,eb/2",
+    hzFrequency: 77.7817
+  },
+  {
+    keyNumber: 20,
+    keyPosition: 11,
+    helmholtzName: "E",
+    scientificName: "E2",
+    letterName: "e/2",
+    hzFrequency: 82.4069
+  },
+  {
+    keyNumber: 21,
+    keyPosition: 12,
+    helmholtzName: "F",
+    scientificName: "F2",
+    letterName: "f/2",
+    hzFrequency: 87.3071
+  },
+  {
+    keyNumber: 22,
+    keyPosition: 12.5,
+    helmholtzName: "F♯/G♭",
+    scientificName: "F♯2/G♭2",
+    letterName: "f#/2,gb/2",
+    hzFrequency: 92.4986
+  },
+  {
+    keyNumber: 23,
+    keyPosition: 13,
+    helmholtzName: "G",
+    scientificName: "G2",
+    letterName: "g/2",
+    hzFrequency: 97.9989
+  },
+  {
+    keyNumber: 24,
+    keyPosition: 13.5,
+    helmholtzName: "G♯/A♭",
+    scientificName: "G♯2/A♭2",
+    letterName: "g#/2,ab/2",
+    hzFrequency: 103.826
+  },
+  {
+    keyNumber: 25,
+    keyPosition: 14,
+    helmholtzName: "A",
+    scientificName: "A2",
+    letterName: "a/2",
+    hzFrequency: 110
+  },
+  {
+    keyNumber: 26,
+    keyPosition: 14.5,
+    helmholtzName: "A♯/B♭",
+    scientificName: "A♯2/B♭2",
+    letterName: "a#/2,bb/2",
+    hzFrequency: 116.541
+  },
+  {
+    keyNumber: 27,
+    keyPosition: 15,
+    helmholtzName: "B",
+    scientificName: "B2",
+    letterName: "b/2",
+    hzFrequency: 123.471
+  },
+  {
+    keyNumber: 28,
+    keyPosition: 16,
+    helmholtzName: "c small octave",
+    scientificName: "C3",
+    letterName: "c/3",
+    hzFrequency: 130.813
+  },
+  {
+    keyNumber: 29,
+    keyPosition: 16.5,
+    helmholtzName: "c♯/d♭",
+    scientificName: "C♯3/D♭3",
+    letterName: "c#/3,db/3",
+    hzFrequency: 138.591
+  },
+  {
+    keyNumber: 30,
+    keyPosition: 17,
+    helmholtzName: "d",
+    scientificName: "D3",
+    letterName: "d/3",
+    hzFrequency: 146.832
+  },
+  {
+    keyNumber: 31,
+    keyPosition: 17.5,
+    helmholtzName: "d♯/e♭",
+    scientificName: "D♯3/E♭3",
+    letterName: "d#/3,eb/3",
+    hzFrequency: 155.563
+  },
+  {
+    keyNumber: 32,
+    keyPosition: 18,
+    helmholtzName: "e",
+    scientificName: "E3",
+    letterName: "e/3",
+    hzFrequency: 164.814
+  },
+  {
+    keyNumber: 33,
+    keyPosition: 19,
+    helmholtzName: "f",
+    scientificName: "F3",
+    letterName: "f/3",
+    hzFrequency: 174.614
+  },
+  {
+    keyNumber: 34,
+    keyPosition: 19.5,
+    helmholtzName: "f♯/g♭",
+    scientificName: "F♯3/G♭3",
+    letterName: "f#/3,gb/3",
+    hzFrequency: 184.997
+  },
+  {
+    keyNumber: 35,
+    keyPosition: 20,
+    helmholtzName: "g",
+    scientificName: "G3",
+    letterName: "g/3",
+    hzFrequency: 195.998
+  },
+  {
+    keyNumber: 36,
+    keyPosition: 20.5,
+    helmholtzName: "g♯/a♭",
+    scientificName: "G♯3/A♭3",
+    letterName: "g#/3,ab/3",
+    hzFrequency: 207.652
+  },
+  {
+    keyNumber: 37,
+    keyPosition: 21,
+    helmholtzName: "a",
+    scientificName: "A3",
+    letterName: "a/3",
+    hzFrequency: 220
+  },
+  {
+    keyNumber: 38,
+    keyPosition: 21.5,
+    helmholtzName: "a♯/b♭",
+    scientificName: "A♯3/B♭3",
+    letterName: "a#/3,bb/3",
+    hzFrequency: 233.082
+  },
+  {
+    keyNumber: 39,
+    keyPosition: 22,
+    helmholtzName: "b",
+    scientificName: "B3",
+    letterName: "b/3",
+    hzFrequency: 246.942
+  },
+  {
+    keyNumber: 40,
+    keyPosition: 23,
+    helmholtzName: "c′ 1-line octave",
+    scientificName: "C4 Middle C",
+    letterName: "c/4",
+    hzFrequency: 261.626
+  },
+  {
+    keyNumber: 41,
+    keyPosition: 23.5,
+    helmholtzName: "c♯′/d♭′",
+    scientificName: "C♯4/D♭4",
+    letterName: "c#/4,db/4",
+    hzFrequency: 277.183
+  },
+  {
+    keyNumber: 42,
+    keyPosition: 24,
+    helmholtzName: "d′",
+    scientificName: "D4",
+    letterName: "d/4",
+    hzFrequency: 293.665
+  },
+  {
+    keyNumber: 43,
+    keyPosition: 24.5,
+    helmholtzName: "d♯′/e♭′",
+    scientificName: "D♯4/E♭4",
+    letterName: "d#/4,eb/4",
+    hzFrequency: 311.127
+  },
+  {
+    keyNumber: 44,
+    keyPosition: 25,
+    helmholtzName: "e′",
+    scientificName: "E4",
+    letterName: "e/4",
+    hzFrequency: 329.628
+  },
+  {
+    keyNumber: 45,
+    keyPosition: 26,
+    helmholtzName: "f′",
+    scientificName: "F4",
+    letterName: "f/4",
+    hzFrequency: 349.228
+  },
+  {
+    keyNumber: 46,
+    keyPosition: 26.5,
+    helmholtzName: "f♯′/g♭′",
+    scientificName: "F♯4/G♭4",
+    letterName: "f#/4,gb/4",
+    hzFrequency: 369.994
+  },
+  {
+    keyNumber: 47,
+    keyPosition: 27,
+    helmholtzName: "g′",
+    scientificName: "G4",
+    letterName: "g/4",
+    hzFrequency: 391.995
+  },
+  {
+    keyNumber: 48,
+    keyPosition: 27.5,
+    helmholtzName: "g♯′/a♭′",
+    scientificName: "G♯4/A♭4",
+    letterName: "g#/4,ab/4",
+    hzFrequency: 415.305
+  },
+  {
+    keyNumber: 49,
+    keyPosition: 28,
+    helmholtzName: "a′",
+    scientificName: "A4 A440",
+    letterName: "a/4",
+    hzFrequency: 440
+  },
+  {
+    keyNumber: 50,
+    keyPosition: 28.5,
+    helmholtzName: "a♯′/b♭′",
+    scientificName: "A♯4/B♭4",
+    letterName: "a#/4,bb/4",
+    hzFrequency: 466.164
+  },
+  {
+    keyNumber: 51,
+    keyPosition: 29,
+    helmholtzName: "b′",
+    scientificName: "B4",
+    letterName: "b/4",
+    hzFrequency: 493.883
+  },
+  {
+    keyNumber: 52,
+    keyPosition: 30,
+    helmholtzName: "c′′ 2-line octave",
+    scientificName: "C5 Tenor C",
+    letterName: "c/5",
+    hzFrequency: 523.251
+  },
+  {
+    keyNumber: 53,
+    keyPosition: 30.5,
+    helmholtzName: "c♯′′/d♭′′",
+    scientificName: "C♯5/D♭5",
+    letterName: "c#/5,db/5",
+    hzFrequency: 554.365
+  },
+  {
+    keyNumber: 54,
+    keyPosition: 31,
+    helmholtzName: "d′′",
+    scientificName: "D5",
+    letterName: "d/5",
+    hzFrequency: 587.33
+  },
+  {
+    keyNumber: 55,
+    keyPosition: 31.5,
+    helmholtzName: "d♯′′/e♭′′",
+    scientificName: "D♯5/E♭5",
+    letterName: "d#/5,eb/5",
+    hzFrequency: 622.254
+  },
+  {
+    keyNumber: 56,
+    keyPosition: 32,
+    helmholtzName: "e′′",
+    scientificName: "E5",
+    letterName: "e/5",
+    hzFrequency: 659.255
+  },
+  {
+    keyNumber: 57,
+    keyPosition: 33,
+    helmholtzName: "f′′",
+    scientificName: "F5",
+    letterName: "f/5",
+    hzFrequency: 698.456
+  },
+  {
+    keyNumber: 58,
+    keyPosition: 33.5,
+    helmholtzName: "f♯′′/g♭′′",
+    scientificName: "F♯5/G♭5",
+    letterName: "f#/5,gb/5",
+    hzFrequency: 739.989
+  },
+  {
+    keyNumber: 59,
+    keyPosition: 34,
+    helmholtzName: "g′′",
+    scientificName: "G5",
+    letterName: "g/5",
+    hzFrequency: 783.991
+  },
+  {
+    keyNumber: 60,
+    keyPosition: 34.5,
+    helmholtzName: "g♯′′/a♭′′",
+    scientificName: "G♯5/A♭5",
+    letterName: "g#/5,ab/5",
+    hzFrequency: 830.609
+  },
+  {
+    keyNumber: 61,
+    keyPosition: 35,
+    helmholtzName: "a′′",
+    scientificName: "A5",
+    letterName: "a/5",
+    hzFrequency: 880
+  },
+  {
+    keyNumber: 62,
+    keyPosition: 35.5,
+    helmholtzName: "a♯′′/b♭′′",
+    scientificName: "A♯5/B♭5",
+    letterName: "a#/5,bb/5",
+    hzFrequency: 932.328
+  },
+  {
+    keyNumber: 63,
+    keyPosition: 36,
+    helmholtzName: "b′′",
+    scientificName: "B5",
+    letterName: "b/5",
+    hzFrequency: 987.767
+  },
+  {
+    keyNumber: 64,
+    keyPosition: 37,
+    helmholtzName: "c′′′ 3-line octave",
+    scientificName: "C6 Soprano C (High C)",
+    letterName: "c/6",
+    hzFrequency: 1046.5
+  },
+  {
+    keyNumber: 65,
+    keyPosition: 37.5,
+    helmholtzName: "c♯′′′/d♭′′′",
+    scientificName: "C♯6/D♭6",
+    letterName: "c#/6,db/6",
+    hzFrequency: 1108.73
+  },
+  {
+    keyNumber: 66,
+    keyPosition: 38,
+    helmholtzName: "d′′′",
+    scientificName: "D6",
+    letterName: "d/6",
+    hzFrequency: 1174.66
+  },
+  {
+    keyNumber: 67,
+    keyPosition: 38.5,
+    helmholtzName: "d♯′′′/e♭′′′",
+    scientificName: "D♯6/E♭6",
+    letterName: "d#/6,eb/6",
+    hzFrequency: 1244.51
+  },
+  {
+    keyNumber: 68,
+    keyPosition: 39,
+    helmholtzName: "e′′′",
+    scientificName: "E6",
+    letterName: "e/6",
+    hzFrequency: 1318.51
+  },
+  {
+    keyNumber: 69,
+    keyPosition: 40,
+    helmholtzName: "f′′′",
+    scientificName: "F6",
+    letterName: "f/6",
+    hzFrequency: 1396.91
+  },
+  {
+    keyNumber: 70,
+    keyPosition: 40.5,
+    helmholtzName: "f♯′′′/g♭′′′",
+    scientificName: "F♯6/G♭6",
+    letterName: "f#/6,gb/6",
+    hzFrequency: 1479.98
+  },
+  {
+    keyNumber: 71,
+    keyPosition: 41,
+    helmholtzName: "g′′′",
+    scientificName: "G6",
+    letterName: "g/6",
+    hzFrequency: 1567.98
+  },
+  {
+    keyNumber: 72,
+    keyPosition: 41.5,
+    helmholtzName: "g♯′′′/a♭′′′",
+    scientificName: "G♯6/A♭6",
+    letterName: "g#/6,ab/6",
+    hzFrequency: 1661.22
+  },
+  {
+    keyNumber: 73,
+    keyPosition: 42,
+    helmholtzName: "a′′′",
+    scientificName: "A6",
+    letterName: "a/6",
+    hzFrequency: 1760
+  },
+  {
+    keyNumber: 74,
+    keyPosition: 42.5,
+    helmholtzName: "a♯′′′/b♭′′′",
+    scientificName: "A♯6/B♭6",
+    letterName: "a#/6,bb/6",
+    hzFrequency: 1864.66
+  },
+  {
+    keyNumber: 75,
+    keyPosition: 43,
+    helmholtzName: "b′′′",
+    scientificName: "B6",
+    letterName: "b/6",
+    hzFrequency: 1975.53
+  },
+  {
+    keyNumber: 76,
+    keyPosition: 44,
+    helmholtzName: "c′′′′ 4-line octave",
+    scientificName: "C7 Double high C",
+    letterName: "c/7",
+    hzFrequency: 2093
+  },
+  {
+    keyNumber: 77,
+    keyPosition: 44.5,
+    helmholtzName: "c♯′′′′/d♭′′′′",
+    scientificName: "C♯7/D♭7",
+    letterName: "c#/7,db/7",
+    hzFrequency: 2217.46
+  },
+  {
+    keyNumber: 78,
+    keyPosition: 45,
+    helmholtzName: "d′′′′",
+    scientificName: "D7",
+    letterName: "d/7",
+    hzFrequency: 2349.32
+  },
+  {
+    keyNumber: 79,
+    keyPosition: 45.5,
+    helmholtzName: "d♯′′′′/e♭′′′′",
+    scientificName: "D♯7/E♭7",
+    letterName: "d#/7,eb/7",
+    hzFrequency: 2489.02
+  },
+  {
+    keyNumber: 80,
+    keyPosition: 46,
+    helmholtzName: "e′′′′",
+    scientificName: "E7",
+    letterName: "e/7",
+    hzFrequency: 2637.02
+  },
+  {
+    keyNumber: 81,
+    keyPosition: 47,
+    helmholtzName: "f′′′′",
+    scientificName: "F7",
+    letterName: "f/7",
+    hzFrequency: 2793.83
+  },
+  {
+    keyNumber: 82,
+    keyPosition: 47.5,
+    helmholtzName: "f♯′′′′/g♭′′′′",
+    scientificName: "F♯7/G♭7",
+    letterName: "f#/7,gb/7",
+    hzFrequency: 2959.96
+  },
+  {
+    keyNumber: 83,
+    keyPosition: 48,
+    helmholtzName: "g′′′′",
+    scientificName: "G7",
+    letterName: "g/7",
+    hzFrequency: 3135.96
+  },
+  {
+    keyNumber: 84,
+    keyPosition: 48.5,
+    helmholtzName: "g♯′′′′/a♭′′′′",
+    scientificName: "G♯7/A♭7",
+    letterName: "g#/7,ab/7",
+    hzFrequency: 3322.44
+  },
+  {
+    keyNumber: 85,
+    keyPosition: 49,
+    helmholtzName: "a′′′′",
+    scientificName: "A7",
+    letterName: "a/7",
+    hzFrequency: 3520
+  },
+  {
+    keyNumber: 86,
+    keyPosition: 49.5,
+    helmholtzName: "a♯′′′′/b♭′′′′",
+    scientificName: "A♯7/B♭7",
+    letterName: "a#/7,bb/7",
+    hzFrequency: 3729.31
+  },
+  {
+    keyNumber: 87,
+    keyPosition: 50,
+    helmholtzName: "b′′′′",
+    scientificName: "B7",
+    letterName: "b/7",
+    hzFrequency: 3951.07
+  },
+  {
+    keyNumber: 88,
+    keyPosition: 51,
+    helmholtzName: "c′′′′′ 5-line octave",
+    scientificName: "C8 Eighth octave",
+    letterName: "c/8",
+    hzFrequency: 4186.01
+  }
+];
