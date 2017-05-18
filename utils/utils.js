a8caee6da34f70bbcfca1ba29ac1f5748bce6a74
yoonster <yowonyoon@gmail.com>
2017-05-18 10:03:49 -0400

    make the pauses smoother

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,7 @@
 const context = new AudioContext();
 const osc = context.createOscillator();
+const gain = context.createGain();
+osc.connect(gain);
 osc.start();
 
 export function updateOscillator(waveType, frequency=440) {
@@ -8,11 +10,17 @@ export function updateOscillator(waveType, frequency=440) {
 }
 
 export function playOscillator() {
-    osc.connect(context.destination);
+    gain.gain.exponentialRampToValueAtTime( 1, context.currentTime + 0.04 );
+    setTimeout(() => {
+        gain.connect(context.destination);
+    }, 0)
 }
 
 export function pauseOscillator() {
-    osc.disconnect(context.destination);
+    gain.gain.exponentialRampToValueAtTime( 0.00001, context.currentTime + 0.04 );
+    setTimeout(() => {
+        gain.disconnect(context.destination);
+    }, 300);
 }
 
 export function randomNumber() {
