4a3832b649c07fb0e5ba7dc4a498ed2924e60b1b
yoonster <yowonyoon@gmail.com>
2017-05-19 15:45:57 -0400

    add button group

--- components/Tone.js
+++ components/Tone.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { updateOscillator, playOscillator, pauseOscillator } from '../utils/utils';
 import { Button, Icon } from 'antd';
 
+const ButtonGroup = Button.Group;
+
 class Tone extends Component {
     componentDidMount() {
         updateOscillator(this.props.waveType, this.props.frequency);
@@ -14,10 +16,12 @@ class Tone extends Component {
 
     render() {
         return (
-            <div>                
-                <Button icon='caret-right' onClick={() => { playOscillator() }} />
-                <Button icon='pause' onClick={() => { pauseOscillator() }} />
-                <Button onClick={this.props.toggle}>start over</Button>
+            <div style={{margin: 5}}>   
+                <ButtonGroup>
+                    <Button icon='caret-right' onClick={() => { playOscillator() }} />
+                    <Button icon='pause' onClick={() => { pauseOscillator() }} />
+                    <Button icon='rollback' onClick={this.props.toggle} />
+                </ButtonGroup>             
             </div>
         );
     }
