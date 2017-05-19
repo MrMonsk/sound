a50749d04bcafb5494e5a8dbc82ec7f027902789
yoonster <yowonyoon@gmail.com>
2017-05-19 16:40:30 -0400

    Select field for mode select

--- containers/Dashboard.js
+++ containers/Dashboard.js
@@ -3,15 +3,43 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { setScaleMode } from '../actions/theory';
 
+import { basicModes } from '../constants/basic';
+
+// UI components
+import { Select } from 'antd';
+const Option = Select.Option;
+
 class Dashboard extends Component {
-    componentDidMount() {
-        this.props.actions.setScaleMode('major');
+    constructor() {
+        super();
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
+    handleInputChange(value) {
+        this.props.actions.setScaleMode(value);
+    }
+    
     render() {
-        const { thoery } = this.props;
+        const { theory } = this.props;
         return (
-            <div>dashboard</div>
+            <div>
+                <h3>Mode Selector</h3>
+                <Select
+                    showSearch
+                    placeholder="Select a scale mode"
+                    optionFilterProp="children"
+                    onChange={this.handleInputChange}
+                    filterOption={(input, option) => option.props.value.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    style={{width: 200}}
+                >
+                    { Object.keys(basicModes).map((mode) => {
+                        return (
+                            <Option key={mode + 'option'} value={mode}>{mode}</Option>
+                        )
+                    })}
+                </Select>
+            </div>
         )
     }
 }
