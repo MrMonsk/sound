60c4fb5425112128f7d0f7c423d28efb13677895
yoonster <yowonyoon@gmail.com>
2017-05-23 19:23:20 -0400

    make the actions

---
+++ actions/timer.js
@@ -0,0 +1,35 @@
+import * as actionTypes from '../constants/actionTypes';
+
+export function startTimer() {
+    return (dispatch) => {
+        clearInterval(timer);
+        timer = setInterval(() => setInterval(() => dispatch(timerTick()), 1000));
+        dispatch({ type: actionTypes.TIMER_START });
+        dispatch(timerTick());
+    }
+}
+
+export function timerTick() {
+    return (dispatch) => {
+        dispatch({
+            type: action.TIMER_TICK
+        });
+    }
+}
+
+export function stopTimer() {
+    return (dispatch) => {
+        clearInterval(timer);
+        dispatch({
+            type: actionTypes.TIMER_STOP
+        });
+    }
+}
+
+export function resetTimer() {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.TIMER_RESET
+        });
+    }
+}
